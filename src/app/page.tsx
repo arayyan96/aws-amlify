4301b02c375d085c1d38f84b6a40d3b57f5e47bd
Abdullah Rayyan <a.rayyan@storyme.info>
2024-08-27 10:56:55 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{"STORYME"}
           </a>
         </div>
       </div>
